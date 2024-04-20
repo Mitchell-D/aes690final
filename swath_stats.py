9ff60c622872c5a387236dfcca4adeab1325d33f
Mitchell-D <mtd0012@uah.edu>
2024-04-20 12:18:39 -0500

    Added some new statistics to the bulk dataset

--- swath_stats.py
+++ swath_stats.py
@@ -15,7 +15,7 @@ if __name__=="__main__":
     swath_stats = []
     ceres_labels = None
 
-    '''
+    #'''
     for p in swath_dir.iterdir():
         print(p.name)
         swath = h5py.File(p.open("rb"), "r")["data"]
@@ -28,29 +28,34 @@ if __name__=="__main__":
 
         X = ceres.data()
         X[X>1.e20] = 0
-        X = np.average(X, axis=0)
-
+        X = np.asarray([
+            np.amin(X, axis=0),
+            np.amax(X, axis=0),
+            np.average(X, axis=0),
+            np.std(X, axis=0),
+            ])
         swath_stats.append((ceres.meta["satellite"], ceres.meta["region"], X))
 
-    satellite,region,averages = zip(*swath_stats)
-    averages = np.stack(averages, axis=0)
-    pkl.dump((ceres_labels, satellite, region, averages), pkl_path.open("wb"))
-    '''
+    satellites,regions,stats = zip(*swath_stats)
+    stats = np.stack(stats, axis=0)
+    pkl.dump((ceres_labels, satellites, regions, stats), pkl_path.open("wb"))
+    exit(0)
+    #'''
 
     #'''
-    ceres_labels, satellite, region, averages = pkl.load(pkl_path.open("rb"))
+    ceres_labels, satellites, regions, averages = pkl.load(pkl_path.open("rb"))
     print(averages.shape)
     #'''
 
-    unique_regions = tuple(set(region))
-    unique_sats = tuple(set(satellite))
+    unique_regions = tuple(set(regions))
+    unique_sats = tuple(set(satellites))
 
     ## Make a data cube with dimensions for (sat, region, datum)
     scube_shape = (len(unique_sats), len(unique_regions), len(ceres_labels))
     scube = np.full(scube_shape, 0.)
 
-    sat_idxs = tuple(unique_sats.index(s) for s in satellite)
-    reg_idxs = tuple(unique_regions.index(r) for r in region)
+    sat_idxs = tuple(unique_sats.index(s) for s in satellites)
+    reg_idxs = tuple(unique_regions.index(r) for r in regions)
     for i,(sidx,ridx) in enumerate(zip(sat_idxs, reg_idxs)):
         scube[sidx,ridx] += averages[i]
     scube /= averages.shape[0]
@@ -61,8 +66,8 @@ if __name__=="__main__":
     '''
     rstats = {r:{s:{l:[] for l in ceres_labels} for s in ("aqua", "terra")}
               for r in unique_regions}
-    isterra = np.array(map(lambda s:s=="terra", satellite))
-    for i,(s,r) in enumerate(zip(satellite,region)):
+    isterra = np.array(map(lambda s:s=="terra", satellites))
+    for i,(s,r) in enumerate(zip(satellites,regions)):
         for j,l in enumerate(ceres_labels):
             rstats[r][s][l].append(averages[i,j])
     '''
